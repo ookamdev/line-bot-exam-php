149c32644ecbedb944d1c76576177b1c534ec679
ookamdev <48230919+ookamdev@users.noreply.github.com>
2019-03-05 10:39:38 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'mkw68nWQSj/b7+M5d1Ay6OobN4426/AZ8HoILU0G9uc8U98xWY/+ZF55Ju4uycaR1uyhKnlVFp8SZp0NEy0vYb8sgUuxiFsQHgprAGzhshkpPS2FRSsjHRLQuNuiKJ522ChPflKTlJ7JmPNE0lTkcQdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Ua1daa27a08dc5e578eabf1d5818a7880';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
